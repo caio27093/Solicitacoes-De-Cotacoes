2c852f50d65d25dca0e7ba320d336408021e7665
caio lima de almeida <caio.lima337@hotmail.com>
2021-07-16 21:11:28 -0300

    Conexão com banco de dados

--- Projeto/Projeto/Controllers/HomeController.cs
+++ Projeto/Projeto/Controllers/HomeController.cs
@@ -6,12 +6,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Projeto.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        MySqlConnection connection = new MySqlConnection ( "DATABASE=PROJETO; port=3306; SERVER=localhost; UID=root; pwd=deidara337;" );
 
         public HomeController ( ILogger<HomeController> logger )
         {
@@ -26,7 +28,18 @@ namespace Projeto.Controllers
 
         public IActionResult Home ( )
         {
-            return View ( );
+            try
+            {
+
+                //connection.Open ( );
+
+                //connection.Close ( );
+                return View ( );
+            }
+            catch (Exception ex)
+            {
+                return View ( );
+            }
         }
 
         public IActionResult Lista ( )
